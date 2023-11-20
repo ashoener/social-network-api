71799dc7c8d7b84c33dee7079b9c497bb3450ee8
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-20 15:20:47 -0600

    Create basic server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from "express";
+import loadRoutes from "./lib/routeLoader";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+await loadRoutes("controllers", app);
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(PORT, "127.0.0.1", () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
